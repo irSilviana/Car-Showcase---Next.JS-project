645e80c823dbb00fc44c10d86186616bcdd2ee52
Irfani Silviana <ir.silviana@gmail.com>
2024-01-20 19:12:17 +0100

    Added Car Card Component

---
+++ utils/index.ts
@@ -0,0 +1,30 @@
+export async function fetchCars() {
+  const headers = {
+    'X-RapidAPI-Key': '567bbc0c30msh7e97c5c84bebd88p1d1871jsn64109fa9fb98',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+  };
+
+  const response = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+    { headers: headers }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
